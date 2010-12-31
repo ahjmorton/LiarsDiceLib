390d0a51c3d5d6fc16b5355df34e8298ed866613
Andrew Morton <ahjmorton@gmail.com>
2010-12-31 17:37:28 +0000

    Added a couple of unit tests to describe behaviour

--- Implementation/game_test.py
+++ Implementation/game_test.py
@@ -15,6 +15,19 @@ class GameDataTest(unittest.TestCase) :
         self.assertTrue(players is not None)
         self.assertEquals(0, len(players))
 
+    def testAddingPlayer(self) :
+        player = Mock(spec=Player)
+        self.subject.add_player(player)
+        players = self.subject.get_players()
+        self.assertTrue(players is not None)
+        self.assertEquals(1, len(players))
+        self.assertTrue(player in players)
+        dice = self.subject.get_dice(player)
+        self.assertTrue(dice is None)
+        bid = self.subject.get_bid(player)
+        self.assertTrue(bid is None)
+
+
 class GameObjectTest(unittest.TestCase) :
     pass
 
